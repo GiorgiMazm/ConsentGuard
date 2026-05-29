c62b4852c3266071590873ad01dbe84cf1209b9a
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2026-05-29 09:21:44 +0200

    place it on bottom

--- extensions/checkout-ui/shopify.d.ts
+++ extensions/checkout-ui/shopify.d.ts
@@ -2,6 +2,6 @@ import '@shopify/ui-extensions';
 
 //@ts-ignore
 declare module './src/Checkout.tsx' {
-  const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+  const shopify: import('@shopify/ui-extensions/purchase.checkout.actions.render-before').Api;
   const globalThis: { shopify: typeof shopify };
 }
